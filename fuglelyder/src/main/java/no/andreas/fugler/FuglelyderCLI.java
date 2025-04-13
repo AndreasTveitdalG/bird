61d399d277c6a9de4c9ae2c7529fdc7dce1967ac
Andreas Vaksdal Tveitdal <andreas.tveitdal@gmail.com>
2025-04-13 13:36:37 +0200

    CLI argument parsing has been moved over to rely on PicoCLI instead of manual parsing

--- fuglelyder/src/main/java/no/andreas/fugler/FuglelyderCLI.java
+++ fuglelyder/src/main/java/no/andreas/fugler/FuglelyderCLI.java
@@ -2,122 +2,133 @@ package no.andreas.fugler;
 
 import java.io.*;
 import java.util.*;
+import java.util.concurrent.Callable;
 import java.util.stream.Stream;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
 import java.lang.InterruptedException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+@Command(
+  name = "fugl",
+  subcommands = {
+    ListCommand.class,
+    DownloadCommand.class,
+    DeleteCommand.class,
+    UpdateCommand.class
+  },
+  mixinStandardHelpOptions = true
+)
 public class FuglelyderCLI extends FuglelyderSkraper {
-  
   public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
     Files.createDirectories(Path.of("birds"));
-    
-    if (args.length < 1) {
-      System.out.println("Select a command to use. Run without argument or subargument for help.");
-      return;
+    int exitCode = new CommandLine(new FuglelyderCLI()).execute(args);
+    System.exit(exitCode);
+    // System.out.println("Select a command to use. Run without argument or subargument for help.");
+  }
+}
+
+// CLI
+@Command(name = "list")
+class ListCommand extends FuglelyderSkraper {
+  @Command(name = "birds")
+  int birds() throws IOException, ClassNotFoundException {
+    // List all birds by ID
+    final List<Bird> birdIndex = getBirdIndex();
+    for (Bird bird : birdIndex) {
+      System.out.println(bird);
     }
-    switch (args[0]) {
-      case "list" -> listCommand(args);
-      case "download" -> downloadCommand(args);
-      case "delete" -> deleteCommand(args);
-      case "update" -> updateCommand(args);
-      default -> {
-        System.out.println("Select a command to use. Run without argument or subargument for help.");
-      }
-    }
+    // TODO: Create a useful exit code
+    return 0;
   }
   
-  // CLI
-  
-  private static void listCommand(String[] args) throws IOException, ClassNotFoundException {
-    if (args.length < 2) {
-      System.out.println("List the pieces of data that are cached");
-      System.out.println("Availible subcommands are `birds, all-sounds, sounds and images`");
-      return;
-    }
-    switch (args[1]) {
-      case "birds" -> {
-        // List all birds by ID
-        final List<Bird> birdIndex = getBirdIndex();
-        for (Bird bird : birdIndex) {
-          System.out.println(bird);
-        }
-      }
-      case "all-sounds" -> {
-        // List all sounds by ID for every bird
-        final List<Bird> birdIndex = getBirdIndex();
-        final HashMap<Bird, List<BirdSound>> soundIndex = getSoundIndex(birdIndex);
-        for (Bird bird : soundIndex.keySet()) {
-          System.out.println(bird);
-          for (BirdSound sound : soundIndex.get(bird)) {
-            System.out.println("\t" + sound);
-          }
-        }
-      }
-      case "sounds" -> {
-        // TODO: extend to take multiple birds at once
-        // List all sounds by ID for one particular bird
-        if (args.length < 3) {
-          System.out.println("List the pieces of data that are cached");
-          System.out.println("Availible subcommands are `birds, all-sounds, sounds and images`");
-          return;
-        }
-        final List<Bird> birdIndex = getBirdIndex();
-        final HashMap<Bird, List<BirdSound>> soundIndex = getSoundIndex(birdIndex);
-        // TODO: Create better method of checking of contains; will always return false as is
-        if (soundIndex.containsKey(new Bird(null, args[2], null))) {
-          for (BirdSound sound : soundIndex.get(new Bird(null, args[2], null))) {
-            System.out.println(sound);
-          }
-        } else {
-          System.out.println("bird `" + args[2] + "` was not found in the cache");
-        }
-      }
-      case "all-images" -> {
-        // List all sounds by ID for every bird
-        final List<Bird> birdIndex = getBirdIndex();
-        final HashMap<Bird, List<BirdImage>> imageIndex = getImageIndex(birdIndex);
-        for (Bird bird : imageIndex.keySet()) {
-          System.out.println(bird);
-          for (BirdImage sound : imageIndex.get(bird)) {
-            System.out.println("\t" + sound);
-          }
-        }
-      }
-      case "images" -> {
-        // TODO: extend to take multiple birds at once
-        // List all sounds by ID for one particular bird
-        if (args.length < 3) {
-          System.out.println("List the pieces of data that are cached");
-          System.out.println("Availible subcommands are `birds, all-sounds, sounds and images`");
-          return;
-        }
-        final List<Bird> birdIndex = getBirdIndex();
-        final HashMap<Bird, List<BirdImage>> imageIndex = getImageIndex(birdIndex);
-        // TODO: Create better method of checking of contains; will always return false as is
-        if (imageIndex.containsKey(new Bird(null, args[2], null))) {
-          for (BirdImage image : imageIndex.get(new Bird(null, args[2], null))) {
-            System.out.println(image);
-          }
-        } else {
-          System.out.println("bird `" + args[2] + "` was not found in the cache");
-        }
-      }
-      default -> {
-        System.out.println("List the pieces of data that are cached");
-        System.out.println("Availible subcommands are `birds, all-sounds, sounds and images`");
+  @Command(name = "all-sounds")
+  int allSounds() throws IOException, ClassNotFoundException {
+    // List all sounds by ID for every bird
+    final List<Bird> birdIndex = getBirdIndex();
+    final HashMap<Bird, List<BirdSound>> soundIndex = getSoundIndex(birdIndex);
+    for (Bird bird : soundIndex.keySet()) {
+      System.out.println(bird);
+      for (BirdSound sound : soundIndex.get(bird)) {
+        System.out.println("\t" + sound);
       }
     }
+    // TODO: Create a useful exit code
+    return 0;
   }
   
-  private static void downloadCommand(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+  @Command(name = "all-images")
+  int allImages() throws IOException, ClassNotFoundException {
+    // List all sounds by ID for every bird
+    final List<Bird> birdIndex = getBirdIndex();
+    final HashMap<Bird, List<BirdImage>> imageIndex = getImageIndex(birdIndex);
+    for (Bird bird : imageIndex.keySet()) {
+      System.out.println(bird);
+      for (BirdImage sound : imageIndex.get(bird)) {
+        System.out.println("\t" + sound);
+      }
+    }
+    // TODO: Create a useful exit code
+    return 0;
+  }
+
+  @Command(name = "sounds")
+  int sounds(@Parameters() String[] birds) throws IOException, ClassNotFoundException {
+    // List all sounds by ID for each bird
+    for (String bird : birds) {
+      final List<Bird> birdIndex = getBirdIndex();
+      final HashMap<Bird, List<BirdSound>> soundIndex = getSoundIndex(birdIndex);
+      // TODO: Create better method of checking of contains; will always return false as is
+      if (soundIndex.containsKey(new Bird(null, bird, null))) {
+        for (BirdSound sound : soundIndex.get(new Bird(null, bird, null))) {
+          System.out.println(sound);
+        }
+      } else {
+        System.out.println("bird `" + bird + "` was not found in the cache");
+      }
+    }
+    // TODO: Create a useful exit code
+    return 0;
+  }
+
+  @Command(name = "images")
+  int images(@Parameters String[] birds) throws IOException, ClassNotFoundException {
+    // List all sounds by ID for each bird
+    for (String bird : birds) {
+      final List<Bird> birdIndex = getBirdIndex();
+      final HashMap<Bird, List<BirdImage>> imageIndex = getImageIndex(birdIndex);
+      // TODO: Create better method of checking of contains; will always return false as is
+      if (imageIndex.containsKey(new Bird(null, bird, null))) {
+        for (BirdImage image : imageIndex.get(new Bird(null, bird, null))) {
+          System.out.println(image);
+        }
+      } else {
+        System.out.println("bird `" + bird + "` was not found in the cache");
+      }
+    }
+    // TODO: Create a useful exit code
+    return 0;
+  }
+  // default -> {
+  //   System.out.println("List the pieces of data that are cached");
+  //   System.out.println("Availible subcommands are `birds, all-sounds, sounds and images`");
+  // }
+}
+
+@Command(name = "download")
+class DownloadCommand extends FuglelyderSkraper implements Callable<Integer> {
+  @Parameters(index = "0")
+  String target;
+
+  @Override
+  public Integer call() throws IOException, InterruptedException, ClassNotFoundException {
     // TODO: enable the possibility of downloading specific sounds and images
-    if (args.length < 2) {
-      System.out.println("Download sounds or images to the cache");
-      System.out.println("Can be done in batch `all-images and all-sounds` or individually for a bird or a particular image/sound");
-      return;
-    }
-    switch (args[1]) {
+    switch (target) {
       case "all-sounds" -> {
         // Download all sounds for all birds
         final List<Bird> birdIndex = getBirdIndex();
@@ -135,16 +146,20 @@ public class FuglelyderCLI extends FuglelyderSkraper {
         System.out.println("Can be done in batch `all-images and all-sounds` or individually for a bird or a particular image/sound");
       }
     }
+    // TODO: Create a useful exit code
+    return 0;
   }
-  
-  private static void deleteCommand(String[] args) throws IOException {
+}
+
+@Command(name = "delete")
+class DeleteCommand extends FuglelyderSkraper implements Callable<Integer> {
+  @Parameters()
+  String target;
+
+  @Override
+  public Integer call() throws IOException {
     // add deletion of induvidual sounds and images
-    if (args.length < 2) {
-      System.out.println("Delete the saved content. Delete the entire save or specific content within it*");
-      System.out.println("  *specific content deletion is not availible for the bird index");
-      return;
-    }
-    switch (args[1]) {
+    switch (target) {
       case "bird-index" -> {
         if (!Files.isDirectory(BIRD_INDEX_SAVE)) {
           System.out.println("Bird index should not be a directory");
@@ -187,15 +202,20 @@ public class FuglelyderCLI extends FuglelyderSkraper {
         System.out.println("  *specific content deletion is not availible for the bird index");
       }
     }
+    // TODO: Create a useful exit code
+    return 0;
   }
-  
-  private static void updateCommand(String[] args) throws IOException, ClassNotFoundException {
+}
+
+@Command(name = "update")
+class UpdateCommand extends FuglelyderSkraper implements Callable<Integer> {
+  @Parameters()
+  String target;
+
+  @Override
+  public Integer call() throws IOException, ClassNotFoundException {
     // create logic
-    if (args.length < 2) {
-      System.out.println("Update the indexes. Specify which to update. `alls-indexes, bird-index, sound-index or image-index`");
-      return;
-    }
-    switch (args[1]) {
+    switch (target) {
       case "all-indexes" -> {
         downloadBirdIndex();
         final List<Bird> birdIndex = readBirdIndex(BIRD_INDEX_SAVE);
@@ -217,5 +237,7 @@ public class FuglelyderCLI extends FuglelyderSkraper {
         System.out.println("Update the indexes. Specify which to update. `alls-indexes, bird-index, sound-index or image-index`");
       }
     }
+    // TODO: Create a useful exit code
+    return 0;
   }
 }
